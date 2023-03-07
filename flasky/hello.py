6bab6b48c7dadf5b8497e1962a506d7ae8d7eb3b
littlephony <uladzislau.lukashevich@gmail.com>
2023-03-07 15:22:49 +0100

    Added basic form sumbmission

--- flasky/hello.py
+++ flasky/hello.py
@@ -1,18 +1,34 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'the world is gonna roll me'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    name = None
+    form = NameForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
     return render_template(
         'index.html', 
-        current_time=datetime.utcnow()
+        form=form,
+        name=name
     )
 
 @app.route('/user/<name>')
