360abcaf4a7aba152e5229e964050bf7a61335dd
littlephony <uladzislau.lukashevich@gmail.com>
2023-03-07 15:27:17 +0100

    Fixed form resubmission confirmation defect

--- flasky/hello.py
+++ flasky/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -18,17 +18,16 @@ moment = Moment(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
 
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
 
     return render_template(
         'index.html', 
         form=form,
-        name=name
+        name=session.get('name')
     )
 
 @app.route('/user/<name>')
